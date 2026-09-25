7a08d0d545e56791be069e44e09215962c28c107
good <133401466+fengpeng12@users.noreply.github.com>
2026-09-26 00:38:09 +0800

    MyAgent: 更新 app/src/main/kotlin/com/example/hello/MainActivity.kt

--- app/src/main/kotlin/com/example/hello/MainActivity.kt
+++ app/src/main/kotlin/com/example/hello/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hello
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
