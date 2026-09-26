1149ed7706809df726c6f84037a1ee87cd66f661
good <133401466+fengpeng12@users.noreply.github.com>
2026-09-26 21:10:09 +0800

    MyAgent: 更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,12 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "HelloApp"
+rootProject.name = "Tasks"
 include(":app")
