395d519c08f376a0ef96de50ef1f3c7579554aa2
good <133401466+fengpeng12@users.noreply.github.com>
2026-09-26 10:42:10 +0800

    MyAgent: 更新 app/src/main/kotlin/com/fengpeng12/firstapp/MainActivity.kt

--- app/src/main/kotlin/com/fengpeng12/firstapp/MainActivity.kt
+++ app/src/main/kotlin/com/fengpeng12/firstapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.fengpeng12.firstapp
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this).apply { text = "  你好!" }
+        val tv = TextView(this).apply {
+            text = "你好!"
+            textSize = 32f
+            setTextColor(Color.BLACK)
+            gravity = Gravity.CENTER
+        }
         setContentView(tv)
     }
 }
